accc38b9443c8e1069365d84830b3b7c187b3362
Darina <dajtbaeva@gmail.com>
2023-04-09 05:29:29 +0600

    uupd

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,23 +9,42 @@
   </form>
 </template>
 <script>
+import { ref } from "vue";
 export default {
-  data() {
-    return {
-      body: "",
-    };
-  },
-  methods: {
-    addTweet() {
-      this.$emit("onSubmit", {
+  emits: ["onSubmit"],
+  setup(props, { emit }) {
+    const body = ref("");
+
+    const addTweet = () => {
+      emit("onSubmit", {
         id: Math.round(Math.random() * 30),
         avatar: `https://avatars.dicebear.com/api/male/${Date.now()}.svg`,
-        body: this.body,
+        body: body.value,
         likes: 0,
         date: new Date(Date.now()).toLocaleString(),
       });
-      this.body = "";
-    },
+      body.value = "";
+    };
+    return { body, addTweet };
   },
 };
+// export default {
+//   data() {
+//     return {
+//       body: "",
+//     };
+//   },
+//   methods: {
+//     addTweet() {
+//       this.$emit("onSubmit", {
+//         id: Math.round(Math.random() * 30),
+//         avatar: `https://avatars.dicebear.com/api/male/${Date.now()}.svg`,
+//         body: this.body,
+//         likes: 0,
+//         date: new Date(Date.now()).toLocaleString(),
+//       });
+//       this.body = "";
+//     },
+//   },
+// };
 </script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,15 @@
 <template>
+  <select v-model="sortBy">
+    <option value="date">Sort by date</option>
+    <option value="likes">Sort by likes</option>
+  </select>
   <ul>
-    <Tweet v-for="item in items" :key="item.id" :item="item"></Tweet>
+    <Tweet v-for="item in sortedList" :key="item.id" :item="item"></Tweet>
   </ul>
 </template>
 <script>
 import Tweet from "@/components/Tweet.vue";
+import { ref, computed } from "vue";
 
 export default {
   components: { Tweet },
@@ -13,6 +18,17 @@ export default {
       type: Array,
       required: true,
     },
+  },
+  setup({ items }) {
+    const sortBy = ref("date");
+    const sortedList = computed(() => {
+      return items.sort((a, b) => {
+        if (a[sortBy.value] > b[sortBy.value]) return -1;
+        if (a[sortBy.value] < b[sortBy.value]) return 1;
+        // new tweets and tweets with more likes at the top
+      });
+    });
+    return { sortBy, sortedList };
   },
 };
 </script>
